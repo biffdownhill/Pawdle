a7177474f017df7c1d82256fc674b490d52ff43d
Edward Downhill <biffdownhill@gmail.com>
2025-06-25 11:08:50 +0100

    feat: add response models

---
+++ Pawdle/Models/AllBreedsResponse.swift
@@ -0,0 +1,11 @@
+//
+//  AllBreedsResponse.swift
+//  Pawdle
+//
+//  Created by Edward Downhill on 24/06/2025.
+//
+
+struct AllBreedsResponse: Codable {
+    let message: [String: [String]]
+    let status: String
+}
---
+++ Pawdle/Models/RandomImageUrlResponse.swift
@@ -0,0 +1,11 @@
+//
+//  AllBreedsResponse.swift
+//  Pawdle
+//
+//  Created by Edward Downhill on 24/06/2025.
+//
+
+struct RandomImageUrlResponse: Codable {
+    let message: String
+    let status: String
+}
